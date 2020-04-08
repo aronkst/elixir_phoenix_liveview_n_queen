964a6d4039c64701528b865c08d5763deae7b747
Aron Stall <aron.0504@hotmail.com>
2020-04-07 22:13:24 -0300

    add deps phoenix_live_view

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule ElixirPhoenixLiveviewNQueen.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:phoenix_live_view, "~> 0.11.0"}
     ]
   end
 end
