7ac73d4ea6513511594c157d1079ecbded79c270
Aron Stall <aron.0504@hotmail.com>
2020-04-07 22:34:51 -0300

    fix comma in config

--- config/config.exs
+++ config/config.exs
@@ -13,7 +13,7 @@ config :elixir_phoenix_liveview_n_queen, ElixirPhoenixLiveviewNQueenWeb.Endpoint
   secret_key_base: "sGfoK39RXsCXT210QU63P4jA64QJA8fYyRwM49kG7ZbtkUqpa4aEdKMnKqRuIT67",
   render_errors: [view: ElixirPhoenixLiveviewNQueenWeb.ErrorView, accepts: ~w(html json)],
   pubsub: [name: ElixirPhoenixLiveviewNQueen.PubSub, adapter: Phoenix.PubSub.PG2],
-  live_view: [signing_salt: "f5uHdeEc"]
+  live_view: [signing_salt: "f5uHdeEc"],
   live_view: [signing_salt: "2I9oJW/b4uQPSJaH9Vv5EkhBHQ4SaJiX"]
 
 # Configures Elixir's Logger
